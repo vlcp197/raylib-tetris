c22b320b496dda91ebf7d50f874bb452fb1c9bd8
vlcp197 <vlcp197@gmail.com>
2025-01-06 21:09:28 -0300

    Add methods to move the blocks and wrap up border detection method

--- game.h
+++ game.h
@@ -5,9 +5,19 @@
 class Game{
     public:
         Game();
+        Block GetRandomBlock();
+        std::vector<Block> GetAllBlocks();
+        void Draw();
+        void HandleInput();
+        void MoveBlockLeft();
+        void MoveBlockRight();
+        void MoveBlockDown();
+        bool IsBlockOutside();
         Grid grid;
 
-
     private:
+        void RotateBlock();
         std::vector<Block> blocks;
+        Block currentBlock;
+        Block nextBlock;
 };
